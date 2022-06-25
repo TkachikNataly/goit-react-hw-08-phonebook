b9416e542faf94b7b36879bf9ac05c10440fa244
TkachikNataly <tkachik.nataly@gmail.com>
2022-06-26 00:04:46 +0300

    PrivateRoute

---
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,18 @@
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { AuthSelector } from 'redux/auth';
+import Container from "components/Container/Container";
+
+
+export default function PrivateRoute({ children }) {
+    const isLoggedIn = useSelector(AuthSelector.getIsLoggedIn);
+    return (
+        <div>
+            {isLoggedIn ? (
+                <Container>{children}</Container>
+            ) : (
+                <Navigate replace to="login" />
+            )}
+        </div>
+    );
+}
