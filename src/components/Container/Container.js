f5830f9f164e6483022ade8c4b07179132ce48ce
TkachikNataly <tkachik.nataly@gmail.com>
2022-06-25 23:23:41 +0300

    creating a container

---
+++ src/components/Container/Container.js
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './Container.module.css';
+import PropTypes from 'prop-types';
+
+
+export default function Container({ children }) {
+    return <div className={s.Container}>{children}</div>;
+}
+
+Container.propTypes = {
+    children: PropTypes.node,
+}
